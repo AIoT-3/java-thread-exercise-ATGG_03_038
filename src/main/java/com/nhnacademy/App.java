4ea72f579d70e7c7ebf38fb82099d175fbe37afc
장재혁 <hyukjjang02@gmail.com>
2026-02-04 16:48:12 +0900

    save

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -18,11 +18,13 @@ import com.nhnacademy.thread.Counter;
 /**
  * 데몬 스레드와 일반 스레드를 활용한 애플리케이션 메인 클래스
  */
+
 public class App {
     public static void main(String[] args) {
         // TODO #5 - AlertDaemon 스레드 객체를 생성하고 start() 메서드를 호출하여 실행합니다.
-        AlertDaemon alertDaemon = null;
+        AlertDaemon alertDaemon = new AlertDaemon();
 
+        alertDaemon.start();
 
         Thread.currentThread().setName("my-thread");
         Counter counter = new Counter(10);
--- src/main/java/com/nhnacademy/thread/AlertDaemon.java
+++ src/main/java/com/nhnacademy/thread/AlertDaemon.java
@@ -11,17 +11,18 @@
  */
 
 package com.nhnacademy.thread;
-
+import lombok.extern.slf4j.Slf4j;
 /**
  * 주기적으로 알림 메시지를 출력하는 데몬 스레드 클래스
  */
+@Slf4j
 public class AlertDaemon extends Thread {
 
     public AlertDaemon() {
         // TODO #1 - setDaemon() 메서드를 사용하여 이 스레드를 데몬 스레드로 설정합니다.
-
+        this.setDaemon(true);
         // TODO #2 - 이 스레드의 이름을 "alert-daemon"으로 설정합니다.
-
+        this.setName("alert-daemon");
 
         // ShutdownHook: JVM이 종료되기 직전에 실행되는 스레드입니다.
         // 프로그램의 정상/비정상 종료 시 필요한 리소스 정리나 로그 기록 등을 처리하며,
@@ -29,6 +30,7 @@ public class AlertDaemon extends Thread {
         Runtime.getRuntime().addShutdownHook(
                 new Thread(() -> {
                     // TODO #3 JVM 종료 시점에 AlertDaemon 스레드가 종료되었음을 알리는 적절한 메시지를 출력합니다.
+                    log.info("JVM종료 -> AlertDaemon 스레드 종료 name: {}, state: {}", AlertDaemon.this.getName(), AlertDaemon.this.getState());
                 })
         );
     }
@@ -36,6 +38,15 @@ public class AlertDaemon extends Thread {
     @Override
     public void run() {
         // TODO #4 1초 간격으로 Alert 데몬 메시지를 지속적으로 출력합니다.
+        while(!isInterrupted()) {
+            try {
+                Thread.sleep(1000);
+                log.info("name: {}, state: {}", Thread.currentThread().getName(), Thread.currentThread().getState());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 
 }
